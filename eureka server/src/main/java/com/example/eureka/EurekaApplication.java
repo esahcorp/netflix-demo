c0f52c73709363f1454a6b838a790dd79112f5a3
esahcorp <lostkite@outlook.com>
2017-09-19 19:50:19 +0800

    删除多余的类，添加注册中心

---
+++ eureka server/src/main/java/com/example/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.example.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by esahcorp on 2017/9/19.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
+    }
+}
